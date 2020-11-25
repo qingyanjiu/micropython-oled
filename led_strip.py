a3d14923c05927d07c56c20afcc298b6b74c50cd
Louis <qingyanjiu@126.com>
2020-11-25 20:21:34 +0800

    更新跑马灯效果

---
+++ led_strip.py
@@ -0,0 +1,18 @@
+import time
+
+import machine
+import neopixel
+
+
+def init_leds():
+    num = 30;
+    np = neopixel.NeoPixel(machine.Pin(5), num)
+    while True:
+        for i in range(num):
+            np[(29 + i) % 30] = (0, 0, 0)
+            np[i % 30] = ((i % 30 + 1) * 8, 0, 0)
+            time.sleep(0.05)
+            np.write()
+
+
+init_leds()
---
+++ oled_and_temp.py
@@ -0,0 +1,58 @@
+import dht
+import network
+from machine import I2C
+from machine import Pin
+
+from ssd1306 import SSD1306_I2C
+
+
+def get_temp_humi():
+    d = dht.DHT11(Pin(5))
+    d.measure()
+    temp = d.temperature()  # eg. 23 (閹虹煰)
+    humi = d.humidity()  # eg. 41 (% RH)
+    return {'temp': temp, 'humi': humi}
+
+
+def get_network():
+    ssid = 'louis_n'
+    passwd = '19831226lc'
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.disconnect()
+    wlan.connect(ssid, passwd)
+    while not wlan.isconnected():
+        time.sleep(1)
+    ip, mask, gateway, dns = wlan.ifconfig()
+    config = {'ip': ip, 'mask': mask, 'gw': gateway, 'dns': dns}
+    return config
+
+
+def show_wifi_text(config, oled):
+    oled.text('IP adress:', 0, 0)
+    oled.text(config['ip'], 0, 16)
+    oled.text('Gate way:', 0, 32)
+    oled.text(config['gw'], 0, 48)
+
+
+def show_temp_humi_text(temp_humi, oled):
+    oled.text('Temperature:', 0, 0)
+    oled.text(str(temp_humi['temp']) + ' C', 0, 16)
+    oled.text('Humidity:', 0, 32)
+    oled.text(str(temp_humi['humi']) + '%', 0, 48)
+
+
+def init_oled():
+    i2c = I2C(scl=Pin(14), sda=Pin(2))
+    oled = SSD1306_I2C(128, 64, i2c)
+    return oled
+
+
+def display(oled, wifi_data, temp_humi_data):
+    oled.fill(0x00)
+    if wifi_data:
+        show_wifi_text(wifi_data, oled)
+    elif temp_humi_data:
+        show_temp_humi_text(temp_humi_data, oled)
+    oled.show()
+
